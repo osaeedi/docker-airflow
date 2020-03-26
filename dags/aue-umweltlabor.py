5a1572494401d4028d15aacd00cbec18a5b16236
jb3-2 <jonas.bieri@gmail.com>
2020-03-26 11:01:10 +0100

    Separate process/upload from ods_update

--- dags/aue-umweltlabor.py
+++ dags/aue-umweltlabor.py
@@ -16,8 +16,8 @@ default_args = {
 }
 
 with DAG('aue-umweltlabor', default_args=default_args, schedule_interval="0 6 * * *", catchup=False) as dag:
-        t1 = DockerOperator(
-                task_id='aue-umweltlabor',
+        process_upload = DockerOperator(
+                task_id='aue-umweltlabor_process-upload',
                 image='aue-umweltlabor:latest',
                 api_version='auto',
                 auto_remove=True,
@@ -29,4 +29,18 @@ with DAG('aue-umweltlabor', default_args=default_args, schedule_interval="0 6 * 
                 volumes=['/mnt/OGD-DataExch/Umweltlabor:/code/data-processing/aue_umweltlabor/data_orig', '/data/dev/workspace/data-processing:/code/data-processing']
         )
 
-        t1
+        ods_publish = DockerOperator(
+                task_id='aue-umweltlabor_ods-publish',
+                image='ods-publish:latest',
+                api_version='auto',
+                auto_remove=True,
+                command='python3 -m ods_publish.etl da_20e9bc,da_uxt6fk,da_q78iuw,da_reclv8',
+                container_name='ods-publish',
+                docker_url="unix://var/run/docker.sock",
+                network_mode="bridge",
+                tty=True,
+                volumes=['/data/dev/workspace/data-processing:/code/data-processing']
+
+        )
+
+        process_upload >> ods_publish
