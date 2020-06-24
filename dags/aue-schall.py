e500aead27e007f46979a88e56f9a4c61305d0d7
jb3-2 <jonas.bieri@gmail.com>
2020-06-24 11:44:04 +0200

    Add aue-schall DAG

---
+++ dags/aue-schall.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+from airflow.operators.docker_operator import DockerOperator
+
+default_args = {
+        'owner'                 : 'jonas.bieri',
+        'description'           : 'Run the aue-schall docker container',
+        'depend_on_past'        : False,
+        'start_date'            : datetime(2020, 6, 24),
+        'email'                 : ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch"],
+        'email_on_failure'      : True,
+        'email_on_retry'        : False,
+        'retries'               : 0,
+        'retry_delay'           : timedelta(minutes=15)
+}
+
+with DAG('covid19bs', default_args=default_args, schedule_interval="*/15 * * * *", catchup=False) as dag:
+        upload = DockerOperator(
+                task_id='upload',
+                image='aue-schall:latest',
+                api_version='auto',
+                auto_remove=True,
+                command='/bin/bash /code/data-processing/aue_schall/etl.sh ',
+                container_name='aue-schall',
+                docker_url="unix://var/run/docker.sock",
+                network_mode="bridge", 
+                tty=True,
+                volumes=['/data/dev/workspace/data-processing:/code/data-processing']
+        )
+
+        ods_publish = DockerOperator(
+                task_id='ods-publish',
+                image='ods-publish:latest',
+                api_version='auto',
+                auto_remove=True,
+                command='python3 -m ods_publish.etl da_f2oh5d',
+                container_name='aue-schall--ods-publish',
+                docker_url="unix://var/run/docker.sock",
+                network_mode="bridge",
+                tty=True,
+                volumes=['/data/dev/workspace/data-processing:/code/data-processing'],
+                retry=2,
+                retry_delay=timedelta(minutes=5)
+        )
+
+        upload >> ods_publish
