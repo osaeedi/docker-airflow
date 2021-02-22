741e3d083f3b189c658f42e0a930b55e62bf026e
jb3-2 <jonas.bieri@gmail.com>
2021-02-22 12:59:55 +0100

    Remove ods_publish - the stations dataset is now pulled regularly by ods

--- dags/aue-schall.py
+++ dags/aue-schall.py
@@ -29,20 +29,20 @@ with DAG('aue-schall', default_args=default_args, schedule_interval="*/15 * * * 
                 volumes=['/data/dev/workspace/data-processing:/code/data-processing']
         )
 
-        ods_publish = DockerOperator(
-                task_id='ods-publish',
-                image='ods-publish:latest',
-                api_version='auto',
-                auto_remove=True,
-                #command='python3 -m ods_publish.etl da_jljxu2,da_nr71j8',
-                command='python3 -m ods_publish.etl_id 100090',
-                container_name='aue_schall--ods-publish',
-                docker_url="unix://var/run/docker.sock",
-                network_mode="bridge",
-                tty=True,
-                volumes=['/data/dev/workspace/data-processing:/code/data-processing'],
-                retry=2,
-                retry_delay=timedelta(minutes=5)
-        )
-
-        upload >> ods_publish
+        # ods_publish = DockerOperator(
+        #         task_id='ods-publish',
+        #         image='ods-publish:latest',
+        #         api_version='auto',
+        #         auto_remove=True,
+        #         #command='python3 -m ods_publish.etl da_jljxu2,da_nr71j8',
+        #         command='python3 -m ods_publish.etl_id 100090',
+        #         container_name='aue_schall--ods-publish',
+        #         docker_url="unix://var/run/docker.sock",
+        #         network_mode="bridge",
+        #         tty=True,
+        #         volumes=['/data/dev/workspace/data-processing:/code/data-processing'],
+        #         retry=2,
+        #         retry_delay=timedelta(minutes=5)
+        # )
+        #
+        # upload >> ods_publish
