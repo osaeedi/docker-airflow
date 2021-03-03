810357a7b2b0829ddd2e7fe0e31ee0259783b1e6
jb3-2 <jonas.bieri@gmail.com>
2021-03-03 11:32:57 +0100

    Rename DAG to bag_coronavirus

--- dags/bag-coronavirus.py
+++ dags/bag-coronavirus.py
@@ -1,5 +1,5 @@
 """
-# bag_coronavirus-tests
+# bag_coronavirus
 This DAG updates the following datasets:
 
 - [100094](https://data.bs.ch/explore/dataset/100094)
@@ -12,7 +12,7 @@ from airflow.operators.docker_operator import DockerOperator
 
 default_args = {
         'owner'                 : 'jonas.bieri',
-        'description'           : 'Run the bag_coronavirus_tests docker container',
+        'description'           : 'Run the bag_coronavirus docker container',
         'depend_on_past'        : False,
         'start_date'            : datetime(2020, 8, 27),
         'email'                 : ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch"],
@@ -22,19 +22,19 @@ default_args = {
         'retry_delay'           : timedelta(minutes=15)
 }
 
-with DAG('bag_coronavirus_tests', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
+with DAG('bag_coronavirus', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
         dag.doc_md = __doc__
         upload = DockerOperator(
                 task_id='upload',
-                image='bag_coronavirus_tests:latest',
+                image='bag_coronavirus:latest',
                 api_version='auto',
                 auto_remove=True,
-                command='/bin/bash /code/data-processing/bag_coronavirus_tests/etl.sh ',
-                container_name='bag_coronavirus_tests',
+                command='/bin/bash /code/data-processing/bag_coronavirus/etl.sh ',
+                container_name='bag_coronavirus',
                 docker_url="unix://var/run/docker.sock",
                 network_mode="bridge", 
                 tty=True,
-                volumes=['/data/dev/workspace/data-processing:/code/data-processing', '/mnt/OGD-DataExch/StatA/BAG_Coronavirus_Tests:/code/data-processing/bag_coronavirus_tests/data']
+                volumes=['/data/dev/workspace/data-processing:/code/data-processing', '/mnt/OGD-DataExch/StatA/BAG_Coronavirus_Tests:/code/data-processing/bag_coronavirus/data']
         )
 
         ods_publish = DockerOperator(
@@ -43,7 +43,7 @@ with DAG('bag_coronavirus_tests', default_args=default_args, schedule_interval="
                 api_version='auto',
                 auto_remove=True,
                 command='python3 -m ods_publish.etl_id 100094,100119',
-                container_name='bag_coronavirus_tests--ods-publish',
+                container_name='bag_coronavirus--ods-publish',
                 docker_url="unix://var/run/docker.sock",
                 network_mode="bridge",
                 tty=True,
