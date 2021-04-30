8879c962813d584d9cd0f265c16e05a26ae5dfae
jb3_2 <jonas.bieri@gmail.com>
2021-04-30 22:35:33 +0200

    Add Task upload_vmdl

--- dags/bag_coronavirus.py
+++ dags/bag_coronavirus.py
@@ -13,45 +13,58 @@ from datetime import datetime, timedelta
 from airflow.operators.docker_operator import DockerOperator
 
 default_args = {
-        'owner'                 : 'jonas.bieri',
-        'description'           : 'Run the bag_coronavirus docker container',
-        'depend_on_past'        : False,
-        'start_date'            : datetime(2020, 8, 27),
-        'email'                 : ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch"],
-        'email_on_failure'      : True,
-        'email_on_retry'        : False,
-        'retries'               : 0,
-        'retry_delay'           : timedelta(minutes=15)
+    'owner'                 : 'jonas.bieri',
+    'description'           : 'Run the bag_coronavirus docker container',
+    'depend_on_past'        : False,
+    'start_date'            : datetime(2020, 8, 27),
+    'email'                 : ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch"],
+    'email_on_failure'      : True,
+    'email_on_retry'        : False,
+    'retries'               : 0,
+    'retry_delay'           : timedelta(minutes=15)
 }
 
 with DAG('bag_coronavirus', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
-        dag.doc_md = __doc__
-        upload = DockerOperator(
-                task_id='upload',
-                image='bag_coronavirus:latest',
-                api_version='auto',
-                auto_remove=True,
-                command='/bin/bash /code/data-processing/bag_coronavirus/etl.sh ',
-                container_name='bag_coronavirus',
-                docker_url="unix://var/run/docker.sock",
-                network_mode="bridge", 
-                tty=True,
-                volumes=['/data/dev/workspace/data-processing:/code/data-processing', '/mnt/OGD-DataExch/StatA/BAG_Coronavirus_Tests:/code/data-processing/bag_coronavirus/data', '/mnt/OGD-DataExch/MD-HMW:/code/data-processing/bag_coronavirus/vmdl_data']
-        )
+    dag.doc_md = __doc__
+    upload = DockerOperator(
+        task_id='upload',
+        image='bag_coronavirus:latest',
+        api_version='auto',
+        auto_remove=True,
+        command='/bin/bash /code/data-processing/bag_coronavirus/etl.sh ',
+        container_name='bag_coronavirus--upload',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        volumes=['/data/dev/workspace/data-processing:/code/data-processing', '/mnt/OGD-DataExch/StatA/BAG_Coronavirus_Tests:/code/data-processing/bag_coronavirus/data']
+    )
 
-        ods_publish = DockerOperator(
-                task_id='ods-publish',
-                image='ods-publish:latest',
-                api_version='auto',
-                auto_remove=True,
-                command='python3 -m ods_publish.etl_id 100094,100119,100116,100123',
-                container_name='bag_coronavirus--ods-publish',
-                docker_url="unix://var/run/docker.sock",
-                network_mode="bridge",
-                tty=True,
-                volumes=['/data/dev/workspace/data-processing:/code/data-processing'],
-                retry=2,
-                retry_delay=timedelta(minutes=5)
-        )
+    upload_vmdl = DockerOperator(
+        task_id='upload_vmdl',
+        image='bag_coronavirus:latest',
+        api_version='auto',
+        auto_remove=True,
+        command='python3 -m bag_coronavirus.etl_vmdl ',
+        container_name='bag_coronavirus--upload_vmdl',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        volumes=['/data/dev/workspace/data-processing:/code/data-processing', '/mnt/OGD-DataExch/MD-HMW:/code/data-processing/bag_coronavirus/vmdl_data']
+    )
 
-        upload >> ods_publish
+    ods_publish = DockerOperator(
+        task_id='ods-publish',
+        image='ods-publish:latest',
+        api_version='auto',
+        auto_remove=True,
+        command='python3 -m ods_publish.etl_id 100094,100119,100116,100123',
+        container_name='bag_coronavirus--ods-publish',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        volumes=['/data/dev/workspace/data-processing:/code/data-processing'],
+        retry=2,
+        retry_delay=timedelta(minutes=5)
+    )
+
+    ods_publish << [upload, upload_vmdl]
