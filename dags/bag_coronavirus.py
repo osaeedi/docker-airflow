fc9b4ca70741563d275c03113c4666c86ad10bf9
jb3_2 <jonas.bieri@gmail.com>
2021-05-11 14:48:20 +0200

    Remove 100111 for now

--- dags/bag_coronavirus.py
+++ dags/bag_coronavirus.py
@@ -58,7 +58,7 @@ with DAG('bag_coronavirus', default_args=default_args, schedule_interval="0 * * 
         image='ods-publish:latest',
         api_version='auto',
         auto_remove=True,
-        command='python3 -m ods_publish.etl_id 100094,100111,100116,100119,100123',
+        command='python3 -m ods_publish.etl_id 100094,100116,100119,100123',
         container_name='bag_coronavirus--ods-publish',
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
