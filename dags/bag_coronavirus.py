05ee15a88a602da05987ed0174fcf3acd1697702
jb3_2 <jonas.bieri@gmail.com>
2021-05-23 10:02:38 +0200

    Temporarily add 9999

--- dags/bag_coronavirus.py
+++ dags/bag_coronavirus.py
@@ -8,6 +8,7 @@ This DAG updates the following datasets:
 - [100119](https://data.bs.ch/explore/dataset/100119)
 - [100123](https://data.bs.ch/explore/dataset/100123)
 - [100135](https://data.bs.ch/explore/dataset/100135)
+- [9999](https://data.bs.ch/explore/dataset/9999)
 """
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
@@ -59,7 +60,7 @@ with DAG('bag_coronavirus', default_args=default_args, schedule_interval="0 * * 
         image='ods-publish:latest',
         api_version='auto',
         auto_remove=True,
-        command='python3 -m ods_publish.etl_id 100094,100111,100116,100119,100123,100135',
+        command='python3 -m ods_publish.etl_id 100094,100111,100116,100119,100123,100135,9999',
         container_name='bag_coronavirus--ods-publish',
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
