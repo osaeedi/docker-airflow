3b073e43aa841bbb0c30a3cdd7da58e0c59a49a6
jb3_2 <jonas.bieri@gmail.com>
2021-06-01 17:50:53 +0200

    Rename DAG, change dataset ids, change Docker container names

--- dags/staka_abstimmungen_live.py
+++ dags/staka_abstimmungen_live.py
@@ -2,8 +2,8 @@
 # staka_abstimmungen
 This DAG updates the 2 datasets that cover the latest polls. At time of this writing, these are:
 
-- [100117](https://data.bs.ch/explore/dataset/100117)
-- [100118](https://data.bs.ch/explore/dataset/100118)
+- [100143](https://data.bs.ch/explore/dataset/100143)
+- [100144](https://data.bs.ch/explore/dataset/100144)
 """
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
@@ -22,7 +22,7 @@ default_args = {
     'retry_delay': timedelta(minutes=3)
 }
 
-with DAG('staka_abstimmungen', default_args=default_args, schedule_interval=None, catchup=False) as dag:
+with DAG('staka_abstimmungen_live', default_args=default_args, schedule_interval=None, catchup=False) as dag:
     dag.doc_md = __doc__
     process_upload = DockerOperator(
         task_id='process-upload',
@@ -30,7 +30,7 @@ with DAG('staka_abstimmungen', default_args=default_args, schedule_interval=None
         api_version='auto',
         auto_remove=True,
         command='/bin/bash /code/data-processing/staka_abstimmungen/etl.sh ',
-        container_name='staka_abstimmungen',
+        container_name='staka_abstimmungen_live',
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
@@ -43,8 +43,8 @@ with DAG('staka_abstimmungen', default_args=default_args, schedule_interval=None
         image='ods-publish:latest',
         api_version='auto',
         auto_remove=True,
-        command='python3 -m ods_publish.etl_id 100117,100118',
-        container_name='staka_abstimmungen--ods-publish',
+        command='python3 -m ods_publish.etl_id 100143,100144',
+        container_name='staka_abstimmungen_live--ods-publish',
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
