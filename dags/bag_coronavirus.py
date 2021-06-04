c5ac22b22748067d6e09fc74e2fb0c1e5008da8f
jb3_2 <jonas.bieri@gmail.com>
2021-06-04 10:52:28 +0200

    Create task upload_impftermine

--- dags/bag_coronavirus.py
+++ dags/bag_coronavirus.py
@@ -56,6 +56,19 @@ with DAG('bag_coronavirus', default_args=default_args, schedule_interval="0 * * 
         volumes=['/data/dev/workspace/data-processing:/code/data-processing', '/mnt/OGD-DataExch/MD-HMW:/code/data-processing/bag_coronavirus/vmdl_data']
     )
 
+    upload_impftermine = DockerOperator(
+        task_id='upload_imptermine',
+        image='bag_coronavirus:latest',
+        api_version='auto',
+        auto_remove=True,
+        command='/bin/bash /code/data-processing/bag_coronavirus/etl_impftermine.sh ',
+        container_name='bag_coronavirus--upload_impftermine',
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        volumes=['/data/dev/workspace/data-processing:/code/data-processing', '/mnt/OGD-DataExch/MD-HMW:/code/data-processing/bag_coronavirus/vmdl_data']
+    )
+
     ods_publish = DockerOperator(
         task_id='ods-publish',
         image='ods-publish:latest',
@@ -71,4 +84,4 @@ with DAG('bag_coronavirus', default_args=default_args, schedule_interval="0 * * 
         retry_delay=timedelta(minutes=5)
     )
 
-    ods_publish << [upload, upload_vmdl]
+    ods_publish << [upload, upload_vmdl, upload_impftermine]
