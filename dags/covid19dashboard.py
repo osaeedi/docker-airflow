e5d03dd5820da492fb583b08ffa90bbef9e63fbc
jb3_2 <jonas.bieri@gmail.com>
2021-09-20 08:59:40 +0200

    Change schedule

--- dags/covid19dashboard.py
+++ dags/covid19dashboard.py
@@ -21,7 +21,7 @@ default_args = {
         'retry_delay'           : timedelta(minutes=15)
 }
 
-with DAG('covid19dashboard', default_args=default_args, schedule_interval="*/10 * * * *", catchup=False) as dag:
+with DAG('covid19dashboard', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
         dag.doc_md = __doc__
         upload = DockerOperator(
                 task_id='upload',
