bd037260e98335cc53e2f5c4c53ae0f942f9941a
jb3_2 <jonas.bieri@gmail.com>
2021-12-27 10:39:00 +0100

    Rename DAG

--- dags/bafu_hydrodata.py
+++ dags/bafu_hydrodata.py
@@ -1,5 +1,5 @@
 """
-# bafu-hydrodata
+# bafu-hydrodaten
 This DAG updates the following datasets:
 
 - [100089](https://data.bs.ch/explore/dataset/100089)
@@ -13,7 +13,7 @@ default_args = {
         'owner'                 : 'jonas.bieri',
         'description'           : 'Run the bafu_hydrodaten docker container',
         'depend_on_past'        : False,
-        'start_date'            : datetime(2021, 5, 15),
+        'start_date'            : datetime(2021, 12, 27),
         'email'                 : ["jonas.bieri@bs.ch", "jonas.eckenfels@bs.ch"],
         'email_on_failure'      : True,
         'email_on_retry'        : False,
@@ -21,7 +21,7 @@ default_args = {
         'retry_delay'           : timedelta(minutes=3)
 }
 
-with DAG('bafu_hydrodata', default_args=default_args, schedule_interval="*/5 * * * *", catchup=False) as dag:
+with DAG('bafu_hydrodaten', default_args=default_args, schedule_interval="*/5 * * * *", catchup=False) as dag:
         dag.doc_md = __doc__
         upload = DockerOperator(
                 task_id='upload',
