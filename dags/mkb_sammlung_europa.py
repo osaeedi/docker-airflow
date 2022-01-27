b9bc05d1ca53c1dca8eadb91e445b34aac31dc3c
hesterfrederiek <hesterpieters@gmail.com>
2022-01-27 17:06:59 +0100

    correct folder name

--- dags/mkb_sammlung_europa.py
+++ dags/mkb_sammlung_europa.py
@@ -22,7 +22,7 @@ default_args = {
     'retry_delay'           : timedelta(minutes=15)
 }
 
-with DAG('gd_coronavirus_abwassermonitoring', default_args=default_args, schedule_interval=None, catchup=False) as dag:
+with DAG('mkb_sammlung_europa', default_args=default_args, schedule_interval=None, catchup=False) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
         task_id='upload',
