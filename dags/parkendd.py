d321a222f286454fc0ed2b57a0ac10023c3b93eb
jb3_2 <jonas.bieri@gmail.com>
2022-02-23 08:30:02 +0100

    Remove task 'ods_publish' (this is now done in the job itself)

--- dags/parkendd.py
+++ dags/parkendd.py
@@ -36,19 +36,19 @@ with DAG('parkendd', default_args=default_args, schedule_interval="0 * * * *", c
         volumes=['/data/dev/workspace/data-processing:/code/data-processing']
     )
 
-    ods_publish = DockerOperator(
-        task_id='ods-publish',
-        image='ods-publish:latest',
-        api_version='auto',
-        auto_remove=True,
-        command='python3 -m ods_publish.etl_id 100014',
-        container_name='parkendd--ods-publish',
-        docker_url="unix://var/run/docker.sock",
-        network_mode="bridge",
-        tty=True,
-        volumes=['/data/dev/workspace/data-processing:/code/data-processing'],
-        retry=5,
-        retry_delay=timedelta(minutes=5)
-    )
-
-    process_upload >> ods_publish
+    # ods_publish = DockerOperator(
+    #     task_id='ods-publish',
+    #     image='ods-publish:latest',
+    #     api_version='auto',
+    #     auto_remove=True,
+    #     command='python3 -m ods_publish.etl_id 100014',
+    #     container_name='parkendd--ods-publish',
+    #     docker_url="unix://var/run/docker.sock",
+    #     network_mode="bridge",
+    #     tty=True,
+    #     volumes=['/data/dev/workspace/data-processing:/code/data-processing'],
+    #     retry=5,
+    #     retry_delay=timedelta(minutes=5)
+    # )
+    #
+    # process_upload >> ods_publish
