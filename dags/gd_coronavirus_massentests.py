eed1b1ac283a86e7a6718f7dbebc3abfbba11883
jb3_2 <jonas.bieri@gmail.com>
2022-03-09 15:06:42 +0100

    Remove schedule for the moment

--- dags/gd_coronavirus_massentests.py
+++ dags/gd_coronavirus_massentests.py
@@ -22,7 +22,7 @@ default_args = {
     'retry_delay'           : timedelta(minutes=15)
 }
 
-with DAG('gd_coronavirus_massentests', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
+with DAG('gd_coronavirus_massentests', default_args=default_args, schedule_interval="None", catchup=False) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
         task_id='upload',
