19b1476a018d45b4cc97b9aa0f3da42a720f02ff
Orhan Saeedi <orhan.saeedi@bs.ch>
2023-11-20 11:32:53 +0100

    Update itbs_klv.py to run DAG every hour

--- dags/itbs_klv.py
+++ dags/itbs_klv.py
@@ -22,7 +22,7 @@ default_args = {
     'retry_delay'           : timedelta(minutes=15)
 }
 
-with DAG('itbs_klv', default_args=default_args, schedule_interval="0 */2 * * *", catchup=False) as dag:
+with DAG('itbs_klv', default_args=default_args, schedule_interval="0 */1 * * *", catchup=False) as dag:
     dag.doc_md = __doc__
     upload_bag_datasets = DockerOperator(
         task_id='upload',
